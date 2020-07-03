fed1064752776483867cfd27af2360aedf0fd617
Julien Hache <julien.hache7@gmail.com>
2020-07-03 16:30:41 +0200

    add ctor and dtor functions for t_darray.

--- source/utils/dynamic_array.c
+++ source/utils/dynamic_array.c
@@ -6,7 +6,7 @@
 /*   By: jhache <jhache@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 13:22:36 by jhache            #+#    #+#             */
-/*   Updated: 2020/07/03 15:07:08 by jhache           ###   ########.fr       */
+/*   Updated: 2020/07/03 16:23:09 by jhache           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,31 @@ static bool		extend(t_darray *arr, size_t size)
 	return (res);
 }
 
+bool			init_darray(t_darray *arr, size_t size)
+{
+	if (size < DARRAY_MIN_SIZE)
+		size = DARRAY_MIN_SIZE;
+	arr->data = malloc(size);
+	if (arr->data == NULL)
+		return (false);
+	arr->capacity = size;
+	arr->size = 0;
+	return (true);
+}
+
+t_darray		create_darray(void)
+{
+	t_darray	res;
+
+	init_darray(&res, DARRAY_MIN_SIZE);
+	return (res);
+}
+
+void			del_darray(t_darray *arr)
+{
+	free(arr->data);
+}
+
 /* may call extend() if necessary (it will double the capacity) */
 bool			append(t_darray *arr, void *data, size_t size)
 {
@@ -45,7 +70,7 @@ bool			append(t_darray *arr, void *data, size_t size)
 	new_size = arr->size + size;
 	if (new_size < arr->size)
 		return (false);
-	if (remaining_size(arr) < size)
+	while (remaining_size(arr) < size)
 	{
 		if (!extend(arr, arr->capacity))
 			return (false);
--- source/utils/dynamic_array.h
+++ source/utils/dynamic_array.h
@@ -6,7 +6,7 @@
 /*   By: jhache <jhache@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 11:55:28 by jhache            #+#    #+#             */
-/*   Updated: 2020/07/03 15:03:49 by jhache           ###   ########.fr       */
+/*   Updated: 2020/07/03 16:29:07 by jhache           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,12 @@ typedef struct		s_dynamic_array
 	size_t			size;
 	size_t			capacity;
 }					t_darray;
+
+# define DARRAY_MIN_SIZE 128
+
+bool		init_darray(t_darray *arr, size_t size);
+t_darray	create_darray(void);
+void		del_darray(t_darray *arr);
 
 bool		append(t_darray *arr, void *data, size_t size);
 
