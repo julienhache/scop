727522b096ff1455b0f394995ab7a9778fbcc759
Julien Hache <julien.hache7@gmail.com>
2020-07-10 03:06:05 +0200

    change dynamic_array files location.

--- source/utils/dynamic_array/dynamic_array.h
+++ source/utils/dynamic_array/dynamic_array.h
@@ -6,12 +6,12 @@
 /*   By: jhache <jhache@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 11:55:28 by jhache            #+#    #+#             */
-/*   Updated: 2020/07/03 16:29:07 by jhache           ###   ########.fr       */
+/*   Updated: 2020/07/09 20:23:06 by jhache           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS_DYNAMIC_ARRAY_H
-# define UTILS_DYNAMIC_ARRAY_H
+#ifndef UTILS_DYNAMIC_ARRAY_DYNAMIC_ARRAY_H
+# define UTILS_DYNAMIC_ARRAY_DYNAMIC_ARRAY_H
 
 # include <stddef.h>
 # include <stdbool.h>
